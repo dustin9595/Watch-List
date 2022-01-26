24cccf9f87237f2b25ef74e2d3cfe1142750b16a
Dustin Dillenburg <dustin.dillenburg@gmail.com>
2022-01-26 02:42:29 +0100

    image upload implemented

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,5 +4,7 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
 
   validates :name, uniqueness: true
-  validates :name, :url, presence: true
+  validates :name, presence: true
+
+  has_one_attached :photo
 end
